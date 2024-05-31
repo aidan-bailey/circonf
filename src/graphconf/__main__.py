66dd277b37fd8110ef1cc93bb48052f76dbb5d9b
Aidan Bailey <aidan.j.bailey@outlook.com>
2024-05-31 14:54:34 +0200

    Progressing

--- src/graphconf/__main__.py
+++ src/graphconf/__main__.py
@@ -1,4 +1,6 @@
 import clingo
+import networkx as nx
+import matplotlib.pyplot as plt
 
 from pprint import pprint
 
@@ -29,36 +31,64 @@ class Context:
 
 
 def main():
-    ctx = Context(10, 10, ["a", "b", "c", "d"], [("a", "b")])
+
+    boundryWidth = 5
+    boundryHeight = 5
+
+    ctx = Context(5, 5, ["a", "b", "c", "d"], [("a", "b")])
     ctl = clingo.Control()
     with open("/Users/aidanbailey/Source/School/graphconf/graphconf.lp", "r") as f:
         code = f.read()
     ctl.add("base", [], code)
 
     def on_model(model):
-        graph = [["  " for row in range(10)] for col in range(10)]
+        #graph = [["  " for row in range(5)] for col in range(5)]
+        nodepositions = {}
+        segments = []
         symbols = model.symbols(atoms=True)
         for symbol in symbols:
             if symbol.name == "node":
                 (x, y, n) = symbol.arguments
-                graph[y.number-1][x.number-1] = '{0: <2}'.format(n.name)
+                nodepositions[n.name] = (x.number - 1, y.number - 1)
             elif symbol.name == "wirestart":
                 (x1, y1, x2, y2, v1, v2) = symbol.arguments
-                graph[y2.number-1][x2.number-1] = "--"
+                segments.append((x1.number - 1, y1.number - 1, x2.number - 1, y2.number - 1, v1, v2))
             elif symbol.name == "wireend":
                 (x1, y1, x2, y2, v1, v2) = symbol.arguments
-                graph[y1.number-1][x1.number-1] = "++"
+                segments.append((x1.number - 1, y1.number - 1, x2.number - 1, y2.number - 1, v1, v2))
             elif symbol.name == "segment":
                 (x1, y1, x2, y2, v1, v2) = symbol.arguments
-                if graph[y1.number-1][x1.number-1] == "  ":
-                    graph[y1.number-1][x1.number-1] = f"{v1.name}{v2.name}"
-                if graph[y2.number-1][x2.number-1] == "  ":
-                    graph[y2.number-1][x2.number-1] = f"{v1.name}{v2.name}"
-            elif symbol.name == "wireLength":
-                print(symbol)
+                segments.append((x1.number - 1, y1.number - 1, x2.number - 1, y2.number - 1, v1, v2))
 
+        G = nx.Graph()
 
-        pprint(graph)
+        pos = {}
+        color = []
+        edges = []
+        labels = {
+
+        }
+
+        for node, (x, y) in nodepositions.items():
+            G.add_node(node)
+            pos[node] = (x, y)
+            labels[node] = node
+            color.append((165/255, 114/255, 169/255, 1))
+
+        for (x1, y1, x2, y2, v1, v2) in segments:
+            pos[f"{v1}{v2},{x1},{y1},{x2},{y2},start"] = (x1, y1)
+            labels[f"{v1}{v2},{x1},{y1},{x2},{y2},start"] = ""
+            pos[f"{v1}{v2},{x1},{y1},{x2},{y2},end"] = (x2, y2)
+            labels[f"{v1}{v2},{x1},{y1},{x2},{y2},end"] = ""
+            color.append((0, 0, 0, 0))
+            color.append((0, 0, 0, 0))
+            G.add_edge(f"{v1}{v2},{x1},{y1},{x2},{y2},start", f"{v1}{v2},{x1},{y1},{x2},{y2},end")
+
+        G.add_edges_from(edges)
+
+        nx.draw(G, pos=pos, node_color = color, labels=labels, with_labels=True, node_size=300, font_size=15)
+
+        plt.show()
 
 
     ctl.ground([("base", [])], context=ctx)
