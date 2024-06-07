9613133aaaa61b9a4aa37ac1a34d7afe3864bde3
Aidan Bailey <dev@aidanbailey.me>
2024-06-07 14:19:27 +0200

    Cleanup

--- src/graphconf/__main__.py
+++ src/graphconf/__main__.py
@@ -1,43 +1,21 @@
-import clingo
-import networkx as nx
-import matplotlib.pyplot as plt
+import os
 
-from pprint import pprint
+import clingo
+import matplotlib.pyplot as plt
+import networkx as nx
+
 
 class Context:
-
-    _boundryWidth: int
-    _boundryHeight: int
-    _vertices: list[str]
-    _edges: list[tuple[str, str]]
-
-    def __init__(self, boundryWidth: int, boundryHeight: int, vertices: list[str], edges: list[tuple[str, str]]) -> None:
-        self._boundryWidth = boundryWidth
-        self._boundryHeight = boundryHeight
-        self._vertices = vertices
-        self._edges = edges
-
-    def boundryWidth(self):
-        return clingo.Number(self._boundryWidth)
-
-    def boundryHeight(self):
-        return clingo.Number(self._boundryHeight)
-
-    def vertices(self):
-        return [clingo.String(v) for v in self._vertices]
-
-    def edges(self):
-        return [clingo.Tuple_((clingo.String(v1), clingo.String(v2))) for (v1, v2) in self._edges]
+    def __init__(
+        self,
+    ) -> None:
+        pass
 
 
 def main():
-
-    boundryWidth = 2
-    boundryHeight = 2
-
-    ctx = Context(5, 5, ["a", "b", "c", "d"], [("a", "b")])
+    ctx = Context()
     ctl = clingo.Control()
-    with open("/Users/aidanbailey/Source/School/graphconf/choosegates.lp", "r") as f:
+    with open(os.path.join(os.curdir, "choosegates.lp"), "r") as f:
         code = f.read()
     ctl.add("base", [], code)
 
@@ -73,7 +51,6 @@ def main():
         labels = {}
 
         for gate, (gate_type) in gate_types.items():
-
             if gate_type == "or_g":
                 gs = "∨"
             elif gate_type == "and_g":
@@ -88,17 +65,21 @@ def main():
             labels[gate] = gs
             G.add_node(gate)
 
-        for (g1, g2) in edges:
+        for g1, g2 in edges:
             G.add_edge(g1, g2)
 
-        graphs.append(lambda: nx.draw(G, labels=labels, with_labels=True, node_size=300, font_size=15))
+        graphs.append(
+            lambda: nx.draw(
+                G, labels=labels, with_labels=True, node_size=300, font_size=15
+            )
+        )
         nx.draw(G, labels=labels, with_labels=True, node_size=300, font_size=15)
 
         plt.show()
 
-
     ctl.ground([("base", [])], context=ctx)
     sol = ctl.solve(on_model=on_model)
+
 
 if __name__ == "__main__":
     main()
