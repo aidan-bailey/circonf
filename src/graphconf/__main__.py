1d1a79ba726260e52bd294c09ea600d3c9e8f031
Aidan Bailey <dev@aidanbailey.me>
2024-06-20 12:25:01 +0200

    Graphs

--- src/graphconf/__main__.py
+++ src/graphconf/__main__.py
@@ -37,9 +37,11 @@ def main():
 
     ctx = Context(5, 5, ["a", "b", "c", "d"], [("a", "b")])
     ctl = clingo.Control()
-    with open("/Users/aidanbailey/Source/School/graphconf/graphconf.lp", "r") as f:
+    with open("/Users/aidanbailey/Source/School/graphconf/graphp/graphp.lp", "r") as f:
         code = f.read()
     ctl.add("base", [], code)
+
+    graphs = []
 
     def on_model(model):
         #graph = [["  " for row in range(5)] for col in range(5)]
@@ -47,17 +49,21 @@ def main():
         segments = []
         symbols = model.symbols(atoms=True)
         for symbol in symbols:
-            if symbol.name == "node":
-                (x, y, n) = symbol.arguments
+            if symbol.name == "place":
+                (c, n) = symbol.arguments
+                (x, y) = c.arguments
                 nodepositions[n.name] = (x.number - 1, y.number - 1)
-            elif symbol.name == "wirestart":
-                (x1, y1, x2, y2, v1, v2) = symbol.arguments
-                segments.append((x1.number - 1, y1.number - 1, x2.number - 1, y2.number - 1, v1, v2))
-            elif symbol.name == "wireend":
-                (x1, y1, x2, y2, v1, v2) = symbol.arguments
-                segments.append((x1.number - 1, y1.number - 1, x2.number - 1, y2.number - 1, v1, v2))
+            #elif symbol.name == "wirestart":
+            #    (x1, y1, x2, y2, v1, v2) = symbol.arguments
+            #    segments.append((x1.number - 1, y1.number - 1, x2.number - 1, y2.number - 1, v1, v2))
+           # elif symbol.name == "wireend":
+           #     (x1, y1, x2, y2, v1, v2) = symbol.arguments
+           #     segments.append((x1.number - 1, y1.number - 1, x2.number - 1, y2.number - 1, v1, v2))
             elif symbol.name == "segment":
-                (x1, y1, x2, y2, v1, v2) = symbol.arguments
+                (e, c1, c2) = symbol.arguments
+                (v1, v2) = e.arguments
+                (x1, y1) = c1.arguments
+                (x2, y2) = c2.arguments
                 segments.append((x1.number - 1, y1.number - 1, x2.number - 1, y2.number - 1, v1, v2))
 
         G = nx.Graph()
@@ -86,13 +92,14 @@ def main():
 
         G.add_edges_from(edges)
 
-        nx.draw(G, pos=pos, node_color = color, labels=labels, with_labels=True, node_size=300, font_size=15)
+        graphs.append(lambda : nx.draw(G, pos=pos, node_color = color, labels=labels, with_labels=True, node_size=300, font_size=15))
 
+    def on_finish(sol):
+        graphs[-1]()
         plt.show()
 
-
     ctl.ground([("base", [])], context=ctx)
-    ctl.solve(on_model=on_model)
+    ctl.solve(on_model=on_model, on_finish=on_finish)
 
 if __name__ == "__main__":
     main()
